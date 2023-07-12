36f8bf6bc63fec19e2bec0433b533263651df207
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-07-12 16:09:31 -0300

    Styles: theme config done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import { Header } from './components/Card/Header'
+import { Header } from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header/>
